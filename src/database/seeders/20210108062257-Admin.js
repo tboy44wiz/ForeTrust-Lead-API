2dbd0c3926fd02500bcca8bca75e9b19c6d21a9e
Osondu Tochukwu <tosolife@yahoo.com>
2021-01-08 11:37:36 +0100

    Added the Migration and Seeding Code for Production 8.
    Took 4 minutes

--- src/database/seeders/20210108062257-Admin.js
+++ src/database/seeders/20210108062257-Admin.js
@@ -12,6 +12,12 @@ module.exports = {
      * }], {});
     */
     await queryInterface.bulkInsert('Staff', [{
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
       staff_name: 'Admin',
       staff_email: 'admin@gmail.com',
       staff_phone: '08033407000',
